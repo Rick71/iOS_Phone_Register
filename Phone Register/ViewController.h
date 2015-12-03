ef59463bc66cdf7f6cf290d8b6c910930a3d1eed
Rick <rvcuag@hotmail.com>
2015-12-03 10:42:17 -0600

    Initial Commit

---
+++ Phone Register/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Phone Register
+//
+//  Created by Ricardo Vera on 03/12/15.
+//  Copyright © 2015 Comercomp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Phone Register/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Phone Register
+//
+//  Created by Ricardo Vera on 03/12/15.
+//  Copyright © 2015 Comercomp. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
